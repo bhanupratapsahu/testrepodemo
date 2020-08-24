764fdcef9ac15e6ca8764fdc429a357b01b0ef72
bhanupratapsahu <bpsahu1@gmail.com>
2020-08-24 14:03:14 +0530

    no message

---
+++ src/main/java/org/atmecs/testrepodemo/App.java
@@ -0,0 +1,13 @@
+package org.atmecs.testrepodemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
